60c3248c8670f6ffd77b15854e94e326e8177386
Patricia <paty4paty@gmail.com>
2020-11-17 17:08:49 -0300

    Inicio

---
+++ backend/src/main/java/pep/distribuidos/DistribuidosApplication.java
@@ -0,0 +1,13 @@
+package pep.distribuidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistribuidosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistribuidosApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/pep/distribuidos/DistribuidosApplicationTests.java
@@ -0,0 +1,13 @@
+package pep.distribuidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistribuidosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
